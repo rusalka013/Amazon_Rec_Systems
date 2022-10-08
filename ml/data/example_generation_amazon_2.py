d334c04f7985e626573f2cff9b043e1aa9edc670
elenaburlando <rusalka013@gmail.com>
2022-10-07 22:53:14 -0700

    change sep

--- ml/data/example_generation_amazon_2.py
+++ ml/data/example_generation_amazon_2.py
@@ -138,7 +138,7 @@ def download_and_extract_data(data_directory,
 def read_data(data_directory, min_rating=None):
   """Read amazon dataset file into dataframe."""
   ratings_df = pd.read_csv(data_directory,
-      sep="::",
+      sep="\t",
       names=RATINGS_DATA_COLUMNS,
       encoding="unicode_escape")  # May contain unicode. Need to escape.
   ratings_df["review_date"] = ratings_df["review_date"].apply(int)
